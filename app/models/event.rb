88d8d466aa5cad5dc78053f2162e057baf0ceee0
kawakubox <shigefumi.kawakubo@gmail.com>
2017-01-16 23:29:21 +0900

    Implements event model

---
+++ app/models/event.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Event
+  attr_reader :doc
+
+  # @param [String] :html Event page html contents
+  def initialize(html:)
+    @doc = Nokogiri::HTML(html)
+  end
+
+  # @return [Array<String>] race page path (without protocol and hostname)
+  def race_urls
+    @doc.search('td a').map do |a|
+      url = a.attr('href')
+      url if url.match(%r{/race/result/.+})
+    end.compact
+  end
+end
