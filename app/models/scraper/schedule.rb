3930778b30afe952efd732d4801265cefa1c366d
kawakubox <shigefumi.kawakubo@gmail.com>
2017-01-23 09:06:59 +0900

    Scrape schedule page

--- app/models/scraper/schedule.rb
+++ app/models/scraper/schedule.rb
@@ -1,18 +1,45 @@
 # frozen_string_literal: true
 
-class Scraper::Schedule
-  attr_reader :doc
+module Scraper
+  class Schedule
+    attr_reader :doc
 
-  # @param [String] :html Schedule page html contents
-  def initialize(html:)
-    @doc = Nokogiri::HTML.parse(html)
-  end
+    # @param [String] :html Schedule page html contents
+    def initialize(html:)
+      @doc = Nokogiri::HTML.parse(html)
+    end
 
-  # @return [Array<String>] event page path (without protocol and hostname)
-  def event_urls
-    @doc.search('td a').map do |a|
-      url = a.attr('href')
-      url if url.match(%r{/race/list/.+})
-    end.compact
+    # @return [Fixnum]
+    def year
+      @doc.search('option[selected]').first.content.match(/(\d+)年/)[1].to_i
+    end
+
+    # @return [Fixnum]
+    def month
+      @doc.search('.scheHeadNaviR strong').first.content.match(/(\d+)月/)[1].to_i
+    end
+
+    # @param [Nokogiri::Node::XML]
+    # @return [Fixnum]
+    def date(tr)
+      tr.at_css('td').child.text.match(/(\d+)日/)[1].to_i
+    end
+
+    # @param [Nokotiri::Node::XML]
+    # @return [Fixnum]
+    def event_id(tr)
+      tr.at_css('td a').attr('href').match(%r{/(\d+)})[1].to_i
+    end
+
+    # @return [Array<Event>]
+    def events
+      @doc.search('tr').map do |tr|
+        tds = tr.search('td')
+        next if tds.blank? || tds.count < 3
+        Event.find_or_create_by(id: event_id(tr)) do |event|
+          event.held_on = Date.new(year, month, date(tr))
+        end
+      end.compact
+    end
   end
 end
--- spec/models/scraper/schedule_spec.rb
+++ spec/models/scraper/schedule_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Scraper::Schedule do
   let(:html) { File.read('spec/fixtures/html/schedule_list.html') }
   subject { Scraper::Schedule.new(html: html) }
 
-  its(:event_urls) { is_expected.to be_present }
-  its('event_urls.size') { is_expected.to eq 24 }
+  its(:year) { is_expected.to eq 2010 }
+  its(:month) { is_expected.to eq 1 }
+
+  describe :events do
+    it 'creates some events' do
+      expect { subject.events }.to change { Event.count }.from(0).to(24)
+    end
+  end
 end
