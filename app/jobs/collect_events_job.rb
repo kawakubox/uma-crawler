abe42d157c8e7d82212eeed2f32e2f65775f90a3
kawakubox <shigefumi.kawakubo@gmail.com>
2017-01-23 22:11:38 +0900

    Create collect events job

---
+++ app/jobs/collect_events_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CollectEventsJob < ApplicationJob
+  queue_as :default
+
+  # @param [Fixnum] :from
+  # @param [Fixnum] :to
+  def perform(from, to)
+    to ||= from
+    years = (from..to).to_a
+    months = (1..12).to_a
+    years.product(months).each do |y, m|
+      url = "http://keiba.yahoo.co.jp/schedule/list/#{y}/?month=#{m}"
+      scraper = Scraper::Schedule.new(html: open(url))
+      scraper.events
+      sleep(5)
+    end
+  end
+end
