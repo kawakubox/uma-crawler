aa82db7360dc123308b0823760ea9a401a785f69
kawakubox <shigefumi.kawakubo@gmail.com>
2017-02-20 23:30:12 +0900

    Change to single event scraping

--- app/jobs/collect_races_job.rb
+++ app/jobs/collect_races_job.rb
@@ -1,11 +1,9 @@
 class CollectRacesJob < ApplicationJob
   queue_as :default
 
-  def perform
-    ::Event.find_each do |event|
-      scraper = Scraper::Event.new(html: open(event.url), event: event)
-      scraper.races
-      sleep(5)
-    end
+  # @param [Event] :event
+  def perform(event)
+    scraper = Scraper::Event.new(html: open(event.url), event: event)
+    scraper.races
   end
 end
