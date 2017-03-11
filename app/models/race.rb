9e1c0c281648b6c10d296f726bf71e4d1ec731ca
kawakubox <shigefumi.kawakubo@gmail.com>
2017-03-11 10:39:01 +0900

    Add callback with before create

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,6 +3,8 @@ class Race < ApplicationRecord
   belongs_to :event
   has_many :refunds
 
+  before_create :set_event
+
   def result_url
     "https://keiba.yahoo.co.jp/race/result/#{id}/"
   end
@@ -13,5 +15,9 @@ class Race < ApplicationRecord
 
   def past_race_results_url
     "https://keiba.yahoo.co.jp/race/denma/#{id}/?page=2"
+  end
+
+  def set_event
+    event = Event.find_or_create_by(id.ts_s[0...-2])
   end
 end
